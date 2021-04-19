7d1ec57bf54dc3b68d3b1162f0603221171deee9
Antonio Preprotnik <preprotnik.novigmail.com>
2021-04-19 20:26:39 +0200

    test

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+</head>
+<body>
+<h2>Index </h2>
+<a href="about.php">Visit about</a>
+<a href="privacy.php">Visit privacy</a>
+</body>
+</html>
